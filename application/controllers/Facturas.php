bee44a80ec9074e6513a4823487cd64d37741687
alguzmank12 <alguzman@k12.com>
2019-07-19 18:29:07 -0500

    Agregado un codigo para obtener los headers que llegan al request

--- application/controllers/Facturas.php
+++ application/controllers/Facturas.php
@@ -9,12 +9,17 @@ class Facturas extends REST_Controller {
     }
 
     public function index_post() {
+
+        //Here make an auth token validation 
+
+        
         $users = [
             ['id' => 1, 'name' => 'John', 'email' => 'john@example.com', 'fact' => 'Loves coding'],
             ['id' => 2, 'name' => 'Jim', 'email' => 'jim@example.com', 'fact' => 'Developed on CodeIgniter'],
             ['id' => 3, 'name' => 'Jane', 'email' => 'jane@example.com', 'fact' => 'Lives in the USA', ['hobbies' => ['guitar', 'cycling']]],
         ];
 
-		$this->response($users, REST_Controller::HTTP_OK);
+       //testing the auth Token:
+       $this->response($this->input->get_request_header('Authorization'));
 	}
 }
---
+++ application/models/User_model.php
@@ -0,0 +1,9 @@
+<?php
+
+class User_model extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
